40e7f704b49fe9d519166606b24687367961b8ee
Bsmiter <151533764+Bsmiter@users.noreply.github.com>
2024-03-03 10:09:01 -0800

    first working copy

---
+++ js.js
@@ -0,0 +1,31 @@
+const container = document.querySelector('#container');
+const btn = document.querySelector('button');
+for (i = 0; i < 128; i++) {
+    const gridDiv = document.createElement('div');
+    gridDiv.setAttribute("id", 'divs')
+    container.appendChild(gridDiv);
+    for (j = 0; j < 128; j++) {
+        const miniDiv = document.createElement('div');
+        miniDiv.setAttribute("id", 'miniDiv')
+        gridDiv.appendChild(miniDiv);
+        miniDiv.addEventListener("click", function (event) {
+            miniDiv.style.backgroundColor = 'red';
+        });
+        var isMousedown = false;
+        miniDiv.addEventListener("mousedown", function (event) {
+            isMousedown = true;
+        })
+        miniDiv.addEventListener("mouseup", function (event) {
+            isMousedown = false;
+        });
+        miniDiv.addEventListener("mousemove", function (event) {
+            if (isMousedown) {
+                miniDiv.style.backgroundColor = 'red';
+            }
+        });
+        btn.addEventListener('click', (e) => { miniDiv.style.backgroundColor = ''; })
+
+
+
+    }
+}
